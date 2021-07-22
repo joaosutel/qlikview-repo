714e8e8e9132431db8e4008fcf27b44f5f073a62
Joao Sutel <joao.sutel@gmail.com>
2021-07-21 21:20:17 -0400

    [add] prepare files to git bash script

--- SCRIPTS/prepare.sh
+++ SCRIPTS/prepare.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
 
-files=$(git status --porcelain | cut -b4-)
-for file in $files; do
-    echo $file
+# Global variables
+qv="C:/Program Files/QlikView/qv.exe"
+rootDir=$(git rev-parse --show-toplevel)
+
+# Git modified files
+gitFiles=$(git status --untracked-files --porcelain | cut -b4-)
+
+# Loop into git files array
+for file in $gitFiles; do
+
+    # Checking if the file was deleted
+    if [ -f "$file" ]; then
+        # File variables
+        fullpath="$rootDir/$file"
+        baseName="$(basename -- $file)"
+        fileName=${file%.*}
+        extension=${file#$fileName.}
+        prjFolder="$fileName-prj"
+        copyFrom="$prjFolder/LoadScript.txt"
+        copyTo=$(echo "$fullpath" | sed "s/$baseName//")
+        
+        # Checking if the file has a qvw extension
+        if [[ $extension = "qvw" ]]; then
+            echo " "
+            echo Preparing "${file^^}" to GitHub
+            echo Creating PRJ folder...
+                mkdir "$prjFolder"
+            echo Removing data from QVW...
+                `"$qv" -r -vvNoData=1 "$fullpath"`
+            echo Copying LoadScript.txt to QVW folder...
+                cp "$copyFrom" -rf "$copyTo" 
+            echo Removing PRJ folder...
+                rm -rf "$prjFolder"
+        fi
+    else 
+        echo "$file was deleted."
+    fi
 done
+
+echo " "
+echo All files are ready to GitHub!
