faff3e60b03c3157ca9b0698d3f3c45aa5323ff3
Joao Sutel <joao.sutel@gmail.com>
2021-07-23 09:43:03 -0400

    Update prepare.sh

--- SCRIPTS/prepare.sh
+++ SCRIPTS/prepare.sh
@@ -6,20 +6,22 @@ rootDir=$(git rev-parse --show-toplevel)
 
 # Git modified files
 gitFiles=$(git status --untracked-files --porcelain | cut -b4-)
+gitFiles="${gitFiles// /*}"
 
 # Loop into git files array
 for file in $gitFiles; do
-
     # Checking if the file was deleted
     if [ -f "$file" ]; then
         # File variables
         fullpath="$rootDir/$file"
-        baseName="$(basename -- $file)"
+        #baseName="$(basename -- $file)"
+		baseName="${file##*/}"
         fileName=${file%.*}
         extension=${file#$fileName.}
         prjFolder="$fileName-prj"
         copyFrom="$prjFolder/LoadScript.txt"
         copyTo=$(echo "$fullpath" | sed "s/$baseName//")
+		copyTo+="[LoadScript]-$baseName.txt"
         
         # Checking if the file has a qvw extension
         if [[ $extension = "qvw" ]]; then
